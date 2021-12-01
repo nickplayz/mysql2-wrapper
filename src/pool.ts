fd0ce818e861cacaa2360e40751692dd6e8bc5ad
nickplayz <nickgameplayz@gmail.com>
2021-12-01 14:59:37 -0300

    refactor: change parsing of mysql config in createConnection

--- src/pool.ts
+++ src/pool.ts
@@ -1,6 +1,4 @@
 import { createPool } from 'mysql2/promise';
-import { parseSemiColons } from './utils';
-import { ConnectionStringParser } from 'connection-string-parser';
 import { connectionString } from './config';
 
 if (!connectionString) {
@@ -8,40 +6,35 @@ if (!connectionString) {
 }
 
 const createConnection = () => {
-    if (connectionString.includes('database=')) {
-        const options: any = parseSemiColons(connectionString);
-        return createPool({
-            host: options.host || 'localhost',
-            port: options.port || 3306,
-            user: options.username || options.user || options.userid || 'root',
-            password: options.password || options.pass || '',
-            database: options.endpoint || options.database,
-            charset: 'utf8mb4_unicode_ci',
-            connectTimeout: 30000,
-            ...options.options,
-            namedPlaceholders: true,
-        });
-    }else{
-        const options = new ConnectionStringParser({
-            scheme: 'mysql',
-            hosts: [],
-        }).parse(connectionString);
+    const isURI = connectionString.includes('mysql://')
 
-        if (Object.keys(options).length === 0) {
-            throw new Error(`Set up mysql_connection_string in correct format - 'mysql://user:password@host/database'`);
-        }
-        return createPool({
-            host: options.hosts[0].host || 'localhost',
-            port: options.hosts[0].port || 3306,
-            user: options.username || 'root',
-            password: options.password || '',
-            database: options.endpoint,
-            charset: 'utf8mb4_unicode_ci',
-            connectTimeout: 30000,
-            ...options.options,
-            namedPlaceholders: true,
-        });
+    if(isURI){
+        return createPool({uri: connectionString})
     }
+
+    const options: any = connectionString
+    .replace(/(?:host(?:name)|ip|server|data\s?source|addr(?:ess)?)=/gi, 'host=')
+    .replace(/(?:user\s?(?:id|name)?|uid)=/gi, 'user=')
+    .replace(/(?:pwd|pass)=/gi, 'password=')
+    .replace(/(?:db)=/gi, 'database=')
+    .split(';')
+    .reduce((connectionInfo, parameter) => {
+      const [key, value] = parameter.split('=');
+      connectionInfo[key] = value;
+      return connectionInfo;
+    }, {});
+
+    return createPool({
+        host: options.host || 'localhost',
+        port: options.port || 3306,
+        user: options.username || options.user || options.userid || 'root',
+        password: options.password || options.pass || '',
+        database: options.endpoint || options.database,
+        charset: 'utf8mb4_unicode_ci',
+        connectTimeout: 30000,
+        ...options.options,
+        namedPlaceholders: true,
+    });
 };
 
 export const pool = createConnection();
